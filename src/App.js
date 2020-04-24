fe5f8c827191dc8b4188e05183c3bb596a1b811b
c-sfeng <clarence.feng@live.com>
2020-04-24 19:44:09 +1000

    More in intro para, and more formatting

--- src/App.js
+++ src/App.js
@@ -27,15 +27,17 @@ function App() {
         <div className="sub-container">
           <p className="primary-text">
             Hi, I’m Clarence, a 2nd year Science and Computer Science
-            student at the University of New South Wales. When I'm not
-            studying or working, I'll be reading or doing photography.
-            I maintain a personal and professional portfolio in 
-            photography, and I'm exploring design and audio-visual
-            production as a Media Director in UNSW's CSESoc. In the future,
-            I hope to work in education, but my current focus is on
-            software engineering. Currently, I am working as a front and back-end intern at Intuit
-            Australia, and I'm also working on some side projects like this website. 
-            If you'd like to know more about what I've done, it's all below.
+            student at the University of New South Wales, majoring in Mathematics and Computer Science. 
+            Additionally, I currently work as an academic tutor, and as a front and back-end software enineer intern at
+            Intuit Australia. 
+          </p>
+          <p className="primary-text">
+            When I'm not studying or working, I'll be reading, watching films or taking photos.
+            In the future, I hope to turn my hobbies into something more, as I 
+            maintain both a personal and professional portfolio in 
+            photography, and I'm exploring design, film-making and audio-visual
+            production not just in a personal capacity, but also as a Media Director in UNSW's Computer Science 
+            and Engineering Society. If you'd like to know more about what I've done, you'll find more details below.
             Thanks for visiting!
           </p>
         </div>
