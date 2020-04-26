6d18a0b0ed98a4f7f11ccdee61c3f81fdb1cc684
c-sfeng <clarence.feng@live.com>
2020-04-26 18:26:46 +1000

    Added test stuff to gallery

--- src/GalleryPage.jsx
+++ src/GalleryPage.jsx
@@ -3,7 +3,15 @@ import { PrimaryBanner } from './PrimaryBanner.jsx';
 import { Header } from './Header.jsx';
 import { Footer } from './Footer.jsx';
 import galleryBanner from './assets/gallery-banner.jpg';
+import test1 from "./assets/gallery/gallery-test-1.jpg";
+import test2 from "./assets/gallery/gallery-test-2.jpg";
+import test3 from "./assets/gallery/gallery-test-3.jpg";
+import test4 from "./assets/gallery/gallery-test-4.jpg";
+import test5 from "./assets/gallery/gallery-test-5.jpg";
+import test6 from "./assets/gallery/gallery-test-6.jpg";
+import test7 from "./assets/gallery/gallery-test-7.jpg";
 import './css/App.scss';
+import './css/GalleryPage.scss';
 
 function GalleryPage() {
     return (
@@ -15,6 +23,29 @@ function GalleryPage() {
                 <PrimaryBanner imgURL={galleryBanner}
                     title="GALLERY"
                     subtitle=""/>
+            </div>
+            <div className="container body-container card-columns" id="personal">
+                <div class="card">
+                    <img src={test1} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test2} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test3} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test4} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test5} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test6} class="card-img-top"></img>
+                </div>
+                <div class="card">
+                    <img src={test7} class="card-img-top"></img>
+                </div>
             </div>
             <div className="container end-container">
                 <Footer/>
