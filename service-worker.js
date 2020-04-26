223bdedc564708bd63f15b7478708879699d0f9d
c-sfeng <clarence.feng@live.com>
2020-04-26 22:02:13 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.eb54c1ad488e29b7ff8e1d255576cabd.js"
+  "/me/precache-manifest.a89787ecc2a513e655357d6294ea90fd.js"
 );
 
 self.addEventListener('message', (event) => {
